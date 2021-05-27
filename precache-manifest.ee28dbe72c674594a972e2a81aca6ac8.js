ccf847eadb4d7befd354eadffd71e66be0ea41ce
Ri-yan <Ri-yan@github.com>
2021-05-27 21:36:53 +0530

    Updates

---
+++ precache-manifest.ee28dbe72c674594a972e2a81aca6ac8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d62a3fba2ac1e1faaf75",
+    "url": "/Ri-yan/SmartBrain/static/css/main.b325b1b5.chunk.css"
+  },
+  {
+    "revision": "d62a3fba2ac1e1faaf75",
+    "url": "/Ri-yan/SmartBrain/static/js/main.3b069c62.chunk.js"
+  },
+  {
+    "revision": "2cef326ce5761668af15",
+    "url": "/Ri-yan/SmartBrain/static/js/runtime~main.8b20223d.js"
+  },
+  {
+    "revision": "81d5106f74cda026c9e4",
+    "url": "/Ri-yan/SmartBrain/static/css/2.8f403d0b.chunk.css"
+  },
+  {
+    "revision": "81d5106f74cda026c9e4",
+    "url": "/Ri-yan/SmartBrain/static/js/2.ebf69caa.chunk.js"
+  },
+  {
+    "revision": "35ffb682c5c832818a71",
+    "url": "/Ri-yan/SmartBrain/static/js/3.299ccf73.chunk.js"
+  },
+  {
+    "revision": "4100098c8af8b04589771aa58935f4c8",
+    "url": "/Ri-yan/SmartBrain/static/media/brain.4100098c.png"
+  },
+  {
+    "revision": "7ee9251b88a7ae979078819711d75800",
+    "url": "/Ri-yan/SmartBrain/index.html"
+  }
+];
