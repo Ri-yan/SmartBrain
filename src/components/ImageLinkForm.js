cda5a28a709c792ce7063208742d12bbdf774f13
Ri-yan <https://ri-yan.github.io/SmartBrain>
2021-06-21 21:32:28 +0530

    front-end completed

--- src/components/ImageLinkForm.js
+++ src/components/ImageLinkForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ImageLinkForm=()=>{
+const ImageLinkForm=({onInputChange,onButtonSubmit})=>{
 	return(
 		<div className="ma3 mt0">
 			<p className="f3">
@@ -8,8 +8,11 @@ const ImageLinkForm=()=>{
 			</p>
 			<div className="center">
 			<div className=" form center glow pa4 br3 shadow-3">
-				<input type="text" className="f4 pa2 w-70 center"/ >
-				<button className="w-30 grow f4 link ph3 bg-light-purple pv2 dib white">Detect</button>
+				<input type="text" className="f4 pa2 w-70 center" onChange={onInputChange}/ >
+				<button 
+				className="w-30 grow f4 link ph3 bg-light-purple pv2 dib white but"
+					onClick={onButtonSubmit}
+				>Detect</button>
 			</div>	
 			</div>	
 		</div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const Navigation=()=>{
-	return(
+const Navigation=({onRouteChange,isSigninedin})=>{
+	
+		if(isSigninedin){
+			return(
 		<nav style={{display:'flex',justifyContent:'flex-end'}}>
-			<p className="f3 link dim black pa3 pointer underline">Sign Out</p>
+			<p onClick={()=>onRouteChange('signout')} className="f3 link dim black pa3 pointer underline">Sign Out</p>
 		</nav>
 		);
+	}else{
+		return(
+		<nav style={{display:'flex',justifyContent:'flex-end'}}>
+			<p onClick={()=>onRouteChange('signin')} className="f3 link dim black pa3 pointer underline">Sign In</p>
+			<p onClick={()=>onRouteChange('register')} className="f3 link dim black pa3 pointer underline">Register</p>
+		</nav>
+		);
+	}
+		
+	
 }
 export default Navigation;
